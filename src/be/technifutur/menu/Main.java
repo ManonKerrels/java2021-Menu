4df8a0e97054c611f0ca49c074d85c231a1c52c4
Yannick <yannick.boogaerts@technifutur.be>
2021-11-25 11:27:17 +0100

    Implémentation de la méthode Main.main

--- src/be/technifutur/menu/Main.java
+++ src/be/technifutur/menu/Main.java
@@ -5,6 +5,7 @@ import be.technifutur.menu.actions.HelloWorld;
 public class Main {
 
     public static void main(String[] args) {
-
+        Runnable action = new HelloWorld();
+        action.run();
     }
 }
