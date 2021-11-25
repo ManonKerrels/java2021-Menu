428006625ea01e66d95568b033ec7c83390476f8
Yannick <yannick.boogaerts@technifutur.be>
2021-11-25 13:15:06 +0100

    appel à l'action NombresPremiers

--- src/be/technifutur/menu/Main.java
+++ src/be/technifutur/menu/Main.java
@@ -1,11 +1,14 @@
 package be.technifutur.menu;
 
 import be.technifutur.menu.actions.HelloWorld;
+import be.technifutur.menu.actions.NombresPremiers;
 
 public class Main {
 
     public static void main(String[] args) {
-        Runnable action = new HelloWorld();
-        action.run();
+        Runnable action1 = new HelloWorld();
+        Runnable action2 = new NombresPremiers();
+        action1.run();
+        action2.run();
     }
 }
